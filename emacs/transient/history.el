22bed12ccc0f8216875d923772719947aa5654ff
Benjamin Trevor <trevorb@nextcapital.com>
2021-04-21 13:35:03 -0500

    emacs libraries

---
+++ emacs/transient/history.el
@@ -0,0 +1,10 @@
+((magit-blame
+  ("-w"))
+ (magit-branch nil)
+ (magit-commit nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate")
+  ("-n256" "--graph" "--decorate" "--stat"))
+ (magit-pull nil)
+ (magit-rebase nil)
+ (magit-submodule nil))
